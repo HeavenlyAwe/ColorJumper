6def5fb8b29d462b32c7ff32a89282594f1dd799
Christoffer <christoffer.fridlund@hotmail.com>
2014-01-25 01:24:53 +0200

    first commit

---
+++ UnityProject/Assets/Scripts/Examples.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Examples : MonoBehaviour {
+
+	public GameObject player;
+	public Transform playersTrans;
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log ("yay2");
+	}
+
+	void Start () {
+		Debug.Log ("yay1");
+	}
+
+	void OnLevelWasLoaded(int arg) {
+		Debug.Log ("yay3");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
