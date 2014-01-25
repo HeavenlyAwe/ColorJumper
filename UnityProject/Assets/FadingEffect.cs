157cab49861c4447487b34632f311a8a36a15f61
Lumianna <ninja.tukiainen@gmail.com>
2014-01-25 18:02:54 +0200

    Fade effect to tiles

---
+++ UnityProject/Assets/FadingEffect.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadingEffect : MonoBehaviour {
+	
+	bool isTweeningOut;
+	bool isTweeningIn;
+	float fadeTime; //in seconds
+
+	float fullAlpha = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		Color color = renderer.material.color;
+		
+		color.a = fullAlpha;
+		renderer.material.color = color;
+		
+		Debug.Log(renderer.material.color.a);
+		
+		isTweeningOut = false;
+		isTweeningIn = false;
+		
+		fadeTime = 2.0f;
+
+		FadeOutTile ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTweeningOut) {
+			Color color = renderer.material.color;
+			
+			color.a -= Time.deltaTime / fadeTime;
+			
+			if (color.a <= 0) {
+				color.a = 0;
+				onFadeOutComplete();
+			}
+			
+			renderer.material.color = color;
+		}
+		else if (isTweeningIn) {
+			Color color = renderer.material.color;
+			
+			color.a += Time.deltaTime / fadeTime;
+			
+			if (color.a >= fullAlpha) {
+				color.a = fullAlpha;
+				onFadeInComplete();
+			}
+			
+			renderer.material.color = color;
+		}
+	}
+	
+	public void FadeOutTile() {
+		isTweeningOut = true;
+		isTweeningIn = false;
+	}
+	
+	public void FadeInTile() {
+		isTweeningIn = true;
+		isTweeningOut = false;
+	}
+	
+	void onFadeOutComplete() {
+		collider.enabled = false;
+		isTweeningOut = false;
+		FadeInTile ();
+	}
+	
+	void onFadeInComplete() {
+		collider.enabled = true;
+		isTweeningIn = false;
+		FadeOutTile ();
+	}
+}
