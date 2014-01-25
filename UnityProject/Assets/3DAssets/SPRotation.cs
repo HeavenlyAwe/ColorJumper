c57cde9c3684d962c545804ea74d13b000bf4187
Anders Forsman <andeye@cyancore.net>
2014-01-25 14:32:58 +0200

    Adding Emilias Art Asset files

---
+++ UnityProject/Assets/3DAssets/SPRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SPRotation : MonoBehaviour {
+	public bool clockwise;
+
+	void Update() {
+		if (clockwise) {
+			transform.RotateAround(transform.position, Vector3.up, 500 * Time.deltaTime);
+		} else {
+			transform.RotateAround(transform.position, Vector3.down, 500 * Time.deltaTime);
+		}
+	}
+}
