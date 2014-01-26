20fad0639dc1c9696485403d3e991b48e98e6cbb
Maim <jhilum@hotmail.com>
2014-01-26 07:02:16 +0200

    Camera zoom works!!

--- UnityProject/Assets/TestingFolder/CameraZoom.cs
+++ UnityProject/Assets/TestingFolder/CameraZoom.cs
@@ -2,51 +2,134 @@
 using System.Collections;
 
 public class CameraZoom : MonoBehaviour {
-
+	
 	public GameObject player1;
 	public GameObject player2;
+	public GameObject player3;
+	public GameObject player4;
 
 	private Vector3 player1position;
 	private Vector3 player2position;
+	private Vector3 player3position;
+	private Vector3 player4position;
+
 	private Vector3 cameraPosition;
 	private Vector3 midPostion;
-
+	private Vector3 meanCenterPoint;
+	
 	private float distanceBetweenCameraNmidpoint;
-	private float distanceBetweenPlayers;
+	//	private float distanceBetweenPlayers1n2;
+	
+	//distance between player3 n distanceBetweenPlayers1n2
+	private float distanceBetweenPlayer3n12;
+	
+	public float zoomLevel;
+	public float smooth=200;
 
-	public float zoomSpeed;
-	public float smooth=5;
+	public int playerAmount ;
 
+	
 	// Use this for initialization
 	void Start () {
-	
-		player1 = GameObject.FindGameObjectWithTag ("Player1");
-		player2 = GameObject.FindGameObjectWithTag ("Player2"); 
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
- 
-		player1position = player1.transform.position;
-		player2position = player2.transform.position;
+		
+		//start of a game zoom out view //not working anymore
+		camera.fieldOfView = Mathf.Lerp (camera.fieldOfView,100, Time.deltaTime * smooth);
+		
+		if(GameObject.Find("Player1")!= null){
+			player1 = GameObject.Find("Player1");
+			player1position = player1.transform.position;
+		}
+		
+		if(GameObject.Find("Player2") != null){
+			player2 = GameObject.Find("Player2"); 
+			player2position = player2.transform.position;
+			playerAmount = 2;
+		}
+		
+		if(GameObject.Find("Player3")!= null){
+			player3 = GameObject.Find("Player3"); 
+			player3position = player3.transform.position;
+			playerAmount = 3;
+		}
 
-	
-		 
-		midPostion = (player1position + player2position) / 2;
-		distanceBetweenCameraNmidpoint = Vector3.Distance (Camera.main.transform.position, midPostion);
+		
+		if(GameObject.Find("Player4")!= null){
+			player4 = GameObject.Find("Player4"); 
+			player4position = player4.transform.position;
+			playerAmount = 4;
+		}
 
-		distanceBetweenPlayers = Vector2.Distance (player1position, player2position);
 
-//		if (distanceBetweenPlayers > 0)
-////		{
-//	 		zoomSpeed = distanceBetweenPlayers / 10;
+		///Find the midpoint to fixate the camera according to players position
+		if (playerAmount == 1) {
+			meanCenterPoint = player1position;
+			//moving the cameras coordinates according to players position
+			Vector3 cameraPos = new Vector3 (meanCenterPoint.x+50, meanCenterPoint.y * 50, meanCenterPoint.z+50);
+			camera.transform.position = Vector3.Lerp (transform.position, cameraPos, Time.deltaTime * smooth);
+			
+			//zooming camera in and out according to players position
+			camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoomLevel, Time.deltaTime * smooth);
+		}
+		
+		if (playerAmount == 2) {
+			meanCenterPoint = (player1position+player2position)/2;
+			//moving the cameras coordinates according to players position
+			Vector3 cameraPos = new Vector3 (meanCenterPoint.x-20, meanCenterPoint.y * 50, meanCenterPoint.z-50);
+			camera.transform.position = Vector3.Lerp (transform.position, cameraPos, Time.deltaTime * smooth);
+			
+			//zooming camera in and out according to players position
+			camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoomLevel, Time.deltaTime * smooth);
+		}
+
+		if (playerAmount == 3) {
+						midPostion = (player1position + player2position) / 2;
+						meanCenterPoint = (player3position + midPostion) / 2;  
+
+						//zoomLevel = midpositionBetweenPlayer3n12/5;
+
+						//moving the cameras coordinates according to players position
+						Vector3 cameraPos = new Vector3 (meanCenterPoint.x-20, meanCenterPoint.y * 50, meanCenterPoint.z-50);
+						camera.transform.position = Vector3.Lerp (transform.position, cameraPos, Time.deltaTime * smooth);
+
+						//zooming camera in and out according to players position
+						camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoomLevel, Time.deltaTime * smooth);
+				}
+
+		if (playerAmount == 4) {
+		
+//			float m = (player1position.x-player4position.x)/(player1position.y-player4position.y);
+//			float constant = player4position.y - m*player4position.x;
+//			//equation for 1 and 4 is 
+//			player4position.y = player4position.x
 //
 //
-//	 		camera.fieldOfView=Mathf.Lerp(camera.fieldOfView,zoomSpeed,Time.deltaTime*smooth);
+//
+//			float m2 = (player2position.x-player3position.x)/(player2position.y-player3position.y);
+//			//equation for 1 and 4 is 
+//			float constant2 = player3position.y - m*player3position.x;
+//            
+
+			meanCenterPoint = (player1position+player2position+player3position+player4position)/4;
+
+			//moving the cameras coordinates according to players position
+			Vector3 cameraPos = new Vector3 (meanCenterPoint.x-20, meanCenterPoint.y * 50, meanCenterPoint.z-100);
+			camera.transform.position = Vector3.Lerp (transform.position, cameraPos, Time.deltaTime * smooth);
+			
+			//zooming camera in and out according to players position
+			camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoomLevel, Time.deltaTime * smooth);
+
+		}
 	 
-// 	    }
-
-	
-	
+	 		
 	}
+//
+//	private Vector3 cameraBoundaries(Vector3 initialCameraPos){
+//
+//
+//	}
 }
