0fb83eb6a8806505a9fd97a53c5ff5d7a46d990f
Christoffer <christoffer.fridlund@hotmail.com>
2014-01-26 14:48:09 +0200

    Menus

--- UnityProject/Assets/Menu/MenuScreen.cs
+++ UnityProject/Assets/Menu/MenuScreen.cs
@@ -7,7 +7,31 @@ public class MenuScreen : MonoBehaviour {
 	public Texture2D tutorialB;
 	public GUIStyle startStyle;
 	public GUIStyle tutorialStyle;
+	public GUIStyle player1S;
+	public GUIStyle player2S;
+	public GUIStyle player3S;
+	public GUIStyle player4S;
+	public Texture2D players;
+	public Texture2D tutorialTexture;
+	private bool player1Selected;
+	private bool player2Selected;
+	private bool player3Selected;
+	private bool player4Selected;
+	public Texture2D player1Active;
+	public Texture2D player1NotAct;
+	public Texture2D player2Active;
+	public Texture2D player2NotAct;
+	public Texture2D player3Active;
+	public Texture2D player3NotAct;
+	public Texture2D player4Active;
+	public Texture2D player4NotAct;
 	private float space = 50;
+
+	private float playerWidth = 128;
+	private float playerHeight = 128;
+
+	private float playersWidth = 512;
+	private float playersHeight = 63;
 
 	private float logoWidth = 1470;
 	private float logoHeight = 680;
@@ -48,27 +72,60 @@ public class MenuScreen : MonoBehaviour {
 		}
 
 		if (seeTutorial) {
-			if (GUI.Button(new Rect (Screen.width/2 - 150, space*1 + logoHeight, 300, 250), "This is tutorial picture")) {
+			if (GUI.Button(new Rect (Screen.width/2 - tutorialTexture.width/4, space*1 + logoHeight/2, tutorialTexture.width/2, tutorialTexture.height/2), tutorialTexture, "")) {
 				seeTutorial = false;
 			}
 		}
 
 		if (choosePlayer) {
-			if (GUI.Button(new Rect (Screen.width/2 - 100,space + logoHeight, 200, 50), "How many players?")) {
+			if (GUI.Button(new Rect (Screen.width/2 - playersWidth/2,space + logoHeight, playersWidth, playersHeight), players ,"")) {
 			}
-			if (GUI.Button(new Rect(Screen.width/2 - 250 - 50, space * 2 + logoHeight + 50, 100, 100), "Player1")) { 
+			if (GUI.Button(new Rect(Screen.width/2 - 250 - 50, space * 2 + logoHeight + 50, playerWidth, playerHeight), "", player1S)) {
+				player1Selected = !player1Selected;
+
+				if (player1Selected) {
+					player1S.normal.background = player1Active;
+				}
+				else {
+					player1S.normal.background = player1NotAct;
+				}
 				Debug.Log("Player 1 chosen");
 			}
-			if (GUI.Button(new Rect(Screen.width/2 - 100 - 50, space * 2 + logoHeight + 50, 100, 100), "Player2")) { 
+			if (GUI.Button(new Rect(Screen.width/2 - 100 - 50, space * 2 + logoHeight + 50, playerWidth, playerHeight), "", player2S)) { 
+				player2Selected = !player2Selected;
+				
+				if (player2Selected) {
+					player2S.normal.background = player2Active;
+				}
+				else {
+					player2S.normal.background = player2NotAct;
+				}
 				Debug.Log("Player 2 chosen");
 			}
-			if (GUI.Button(new Rect(Screen.width/2 + 100 - 50, space * 2 + logoHeight + 50, 100, 100), "Player3")) {
+			if (GUI.Button(new Rect(Screen.width/2 + 100 - 50, space * 2 + logoHeight + 50, playerWidth, playerHeight), "", player3S)) {
+				player3Selected = !player3Selected;
+				
+				if (player3Selected) {
+					player3S.normal.background = player3Active;
+				}
+				else {
+					player3S.normal.background = player3NotAct;
+				}
 				Debug.Log("Player 3 chosen");
 			}
-			if (GUI.Button(new Rect(Screen.width/2 + 250 - 50, space * 2 + logoHeight + 50, 100, 100), "Player4")) {
+			if (GUI.Button(new Rect(Screen.width/2 + 250 - 50, space * 2 + logoHeight + 50, playerWidth, playerHeight), "", player4S)) {
+				player4Selected = !player4Selected;
+				
+				if (player4Selected) {
+					player4S.normal.background = player4Active;
+				}
+				else {
+					player4S.normal.background = player4NotAct;
+				}
 				Debug.Log("Player 4 chosen");
 			}
 			if (GUI.Button(new Rect (Screen.width/2 - startWidth/4 ,space * 3 + logoHeight + 150, startWidth/2, startHeight/2), "", startStyle)) {
+				Application.LoadLevel(0);
 				Debug.Log("start game!");
 			}
 		}
